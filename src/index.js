c0dfad0383519507c8fc527ba3a89474e0297a1d
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2021-04-12 22:58:01 +0200

    env

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// import './styles.css';
+
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const body = document.querySelector('body');
+body.style.background = colors[randomIntegerFromInterval(0, colors.length)];
+
