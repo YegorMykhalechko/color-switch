b4a7aaa21959bed7ab6ca5cd9cfae937a857d5a7
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2021-04-13 15:57:16 +0200

    ghp_color

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import './styles.css';
+import './styles.css';
 
 const colors = [
     '#FFFFFF',
@@ -9,12 +9,33 @@ const colors = [
     '#795548',
 ];
 
-
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('[data-action="start"]'),
+    btnStop: document.querySelector('[data-action="stop"]')
+}
 
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
+const randomColor = () => refs.body.style.background = colors[randomIntegerFromInterval(0, colors.length)];
 
-const body = document.querySelector('body');
-body.style.background = colors[randomIntegerFromInterval(0, colors.length)];
 
+const random = {
+    timerId: null,
+    activeRandom: false,
+
+    start() {
+        if (this.activeRandom) return;
+        this.activeRandom = true;
+        this.timerId = setInterval(randomColor, 1000)
+    },
+    stop() {
+        this.activeRandom = false;
+        clearInterval(this.timerId);
+    }
+}
+
+
+refs.btnStart.addEventListener('click', random.start.bind(random))
+refs.btnStop.addEventListener('click', random.stop.bind(random))
